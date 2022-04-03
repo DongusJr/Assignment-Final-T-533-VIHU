ee436e2a98ee32726a69c5a15015e377b97bcdca
Dongus <bjarkim19@ru.is>
2022-04-03 22:49:05 +0000

    Added datefns, it does not work, so temporarilly use moment

--- utils/dateUtils.ts
+++ utils/dateUtils.ts
@@ -1,6 +1,10 @@
 import * as datefns from "date-fns";
 import moment from "moment";
 
-export function getTimeFromNow(createdAt: Date){
-    return datefns.formatDistanceToNow(createdAt)
+export function getTimeFromNow(createdAt?: Date) : string{
+    if (createdAt != undefined) {
+        // return datefns.formatDistanceToNow(createdAt);
+        return moment(createdAt).fromNow();
+    }
+    return "";
 }
